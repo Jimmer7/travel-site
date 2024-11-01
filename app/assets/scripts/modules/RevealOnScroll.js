8075f7949567735774617bdce0d4255bc1cd5af9
Jim <jimmer_7@hotmail.com>
2024-11-01 13:04:58 -0700

    Completed Sticky header functionality

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -14,39 +14,38 @@ class RevealOnScroll {
   events() {
     window.addEventListener("scroll", this.scrollThrottle)
     window.addEventListener("resize", debounce(() => {
-        console.log("Rezise just ran")
-        this.browserHeight = window.innerHeight
+      console.log("Resize just ran")
+      this.browserHeight = window.innerHeight
     }, 333))
   }
 
   calcCaller() {
     console.log("Scroll function ran")
-        this.itemsToReveal.forEach( el => {
-            if (el.isRevealed == false) {
-                this.calculateIfScrolledTo(el)
-            }
-        })
+    this.itemsToReveal.forEach(el => {
+      if (el.isRevealed == false) {
+        this.calculateIfScrolledTo(el)
+      }
+    })
   }
 
   calculateIfScrolledTo(el) {
-    if(window.scrollY + this.browserHeight > el.offsetTop) {
-        console.log("Element was calculated")
-        /*    console.log(el.getBoundingClientRect().top) */    
-        let scrollPercent = (el.getBoundingClientRect().y / this.browserHeight) * 100
-        if (scrollPercent < this.thresholdPercent) {
-            el.classList.add("reveal-item--is-visible")
-            el.isRevealed = true
-            if(el.isLastItem) {
-                window.removeEventListener("scroll", this.scrollThrottle)
-            }
+    if (window.scrollY + this.browserHeight > el.offsetTop) {
+      console.log("Element was calculated")
+      let scrollPercent = (el.getBoundingClientRect().top / this.browserHeight) * 100
+      if (scrollPercent < this.thresholdPercent) {
+        el.classList.add("reveal-item--is-visible")
+        el.isRevealed = true
+        if (el.isLastItem) {
+          window.removeEventListener("scroll", this.scrollThrottle)
         }
+      }
     }
   }
 
   hideInitially() {
-    this.itemsToReveal.forEach(el =>  {
-        el.classList.add("reveal-item")  
-        el.isRevealed = false
+    this.itemsToReveal.forEach(el => {
+      el.classList.add("reveal-item")
+      el.isRevealed = false
     })
     this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true
   }
