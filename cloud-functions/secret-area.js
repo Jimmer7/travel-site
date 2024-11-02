ae50a8a87f549b183ca6a19d7e143b2340646f51
Jim <jimmer_7@hotmail.com>
2024-11-02 14:34:25 -0700

    Updated cloud function to theck password.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,28 @@
 exports.handler = function(event, context, callback) {
+    const secretContent = `
+    <h3>Welcome to the secret Area </h3>
+    <p>Here we can tell you that the sky is <strong>blue</Strong>, and two plus tow equals four.</p>
+    `
+
+    let body
+    
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+if (body.password == "javascript" ){
+    callback(null, {
+        statusCode: 200,
+        body: secretContent
+    })
+} else {
+    callback(null, {
+        statusCode: 401
+    })
+}
+
     callback(null, {
         statusCode: 200,
         body: "Welcome to the super secret area"
